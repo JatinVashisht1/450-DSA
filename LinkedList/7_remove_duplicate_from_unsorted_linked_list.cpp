55cc47a1de2ade344d73f145d33153cde448746b
Jatin <jatin8085vashisht@gmail.com>
2022-04-09 00:16:55 +0530

    remove duplicate from unsorted linked list

---
+++ LinkedList/7_remove_duplicate_from_unsorted_linked_list.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+void insert(Node **head, int data, int pos)
+{
+    Node *temp = new Node();
+    temp->data = data;
+    if (pos == 1)
+    {
+        temp->next = *head;
+        *head = temp;
+        return;
+    }
+
+    Node *temp2 = new Node();
+    temp2 = *head;
+    for (int i = 0; i < pos - 2; i++)
+    {
+        temp2 = temp2->next;
+    }
+
+    temp->next = temp2->next;
+    temp2->next = temp;
+    return;
+}
+
+void printList(Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    cout << head->data << " ";
+    printList(head->next);
+}
+
+Node *removeDuplicateFromUnsortedList(Node *head)
+{
+    unordered_set<int> set;
+    Node *current = head;
+    Node *previous = NULL;
+    while (current != nullptr)
+    {
+        if (set.find(current->data) != set.end())
+        {
+            previous->next = current->next;
+        }
+        else
+        {
+            set.insert(current->data);
+            previous = current;
+        }
+        current = current->next;
+    }
+    return head;
+}
+
+int main()
+{
+    Node *head = NULL;
+    int numberOfElements = 12;
+
+    for (int i = 1; i <= numberOfElements; i++)
+    {
+        int data = i + 1;
+        if (i == 9 || i == 11)
+        {
+            data = 9;
+        }
+        insert(&head, data, i);
+    }
+    printList(head);
+    cout<<"\n";
+    head = removeDuplicateFromUnsortedList(head);
+    printList(head);
+    return 0;
+}
